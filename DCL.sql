31e971ecc19d558d9f0b79a3031e87378f2c2fee
Ignacio <nac0530002@gmail.com>
2026-05-06 18:01:11 -0300

    Update DCL.sql

---
+++ DCL.sql
@@ -0,0 +1,18 @@
+CREATE USER adminadmin WITH PASSWORD "1234";
+
+CREATE USER gerente_calidad WITH PASSWORD "1234";
+
+CREATE USER inspector WITH PASSWORD "1234";
+
+CREATE USER compras WITH PASSWORD "1234";
+
+CREATE USER auditor WITH PASSWORD "1234";
+
+GRANT ALL ON produccion TO gerente_calidad;
+
+GRANT SELECT, INSERT ON control_calidad TO inspector,
+      SELECT ON lotes, materias_primas TO inspector;
+
+GRANT SELECT, INSERT ON proveedores, orden_compra, lote_materia_prima, recepcion TO compras;
+
+GRANT SELECT ON vista.lista TO auditor; 
