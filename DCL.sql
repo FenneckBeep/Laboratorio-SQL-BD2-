69042a66ac5d5993eedfbbdcd5e01c24142fa251
Ignacio <nac0530002@gmail.com>
2026-05-06 18:24:34 -0300

    Update DCL.sql

--- DCL.sql
+++ DCL.sql
@@ -11,7 +11,8 @@ CREATE USER auditor WITH PASSWORD '1234';
 GRANT ALL ON produccion TO gerente_calidad;
 
 GRANT SELECT, INSERT ON control_calidad TO inspector,
-      SELECT ON lotes, materias_primas TO inspector;
+
+GRANT SELECT ON lotes, materias_primas TO inspector;
 
 GRANT SELECT, INSERT ON proveedores, orden_compra, lote_materia_prima, recepcion TO compras;
 
