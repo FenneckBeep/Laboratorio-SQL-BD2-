b746c06fd9f553e07c0042a315aac7001f2fcb88
Fenneck0-Beep <benrod27t@gmail.com>
2026-06-15 14:48:45 -0300

    Update vista_query07.sql

--- Lab_corregido/vista_query07.sql
+++ Lab_corregido/vista_query07.sql
@@ -1,3 +1,6 @@
+-- ============================================================================
+-- Vista Materialzada (Con Natural Joins) 
+-- ============================================================================
 CREATE MATERIALIZED VIEW informe_2025 AS
 SELECT  p.nombre AS proveedor,
         COUNT(l.id_lote) AS total_lotes_recibidos,
@@ -12,3 +15,9 @@ LEFT JOIN control_de_calidad c ON l.id_lote = c.id_lote AND c.id_estado = 1
 LEFT JOIN motivo_rechazo m ON c.id_rechazo = m.id_rechazo
 WHERE r.fecha_recepcion >= '2025-01-01' AND r.fecha_recepcion <= '2025-12-31'
 GROUP BY p.id_proveedor, p.nombre;
+
+
+-- ============================================================================
+-- Vista Materialzada (Con Joins) 
+-- ============================================================================
+
