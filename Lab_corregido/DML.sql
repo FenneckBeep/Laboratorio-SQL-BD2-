b256ff77b5962d52a5db69c6c7587548ae9f2978
Fenneck0-Beep <benrod27t@gmail.com>
2026-07-02 12:23:30 -0300

    Update DML.sql

--- Lab_corregido/DML.sql
+++ Lab_corregido/DML.sql
@@ -68,20 +68,14 @@ INSERT INTO Tipo_Control (ID_TipoControl, Nombre) VALUES
 -- 2. ESPECIALIZACIONES
 -- ============================================================================
 
-INSERT INTO Cuantitativo
-(ID_TipoControl, Unidad_de_Medida, Valor_Min, Valor_Max)
-VALUES
-(1, 'Escala pH', 6.40, 6.80),
-(2, '% Porcentaje', 0.00, 5.00);
+INSERT INTO Cualitativo (ID_TipoControl) VALUES
+(3),
+(4);
 
-INSERT INTO Integridad_Envase (ID_Integridad, Nombre_Integridad) VALUES
-(1, 'Excelente'),
-(2, 'Normal'),
-(3, 'Defectuoso');
-
-INSERT INTO Cualitativo (ID_TipoControl, ID_Integridad) VALUES
-(3, 2), AAAAAAAAAAAAAAAA
-(4, 1); AAAAAAAAAAAAAAAAA
+INSERT INTO Integridad_Envase VALUES
+(1,'Excelente'),
+(2,'Normal'),
+(3,'Defectuoso');
 
 
 -- ============================================================================
