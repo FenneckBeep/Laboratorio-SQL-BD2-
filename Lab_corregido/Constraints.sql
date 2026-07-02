fd4c686005ca1b4bd52aec1ff4a0bd70e87c0d47
Fenneck0-Beep <benrod27t@gmail.com>
2026-07-02 15:26:45 -0300

    Update Constraints.sql

--- Lab_corregido/Constraints.sql
+++ Lab_corregido/Constraints.sql
@@ -120,6 +120,12 @@ FOREIGN KEY (ID_TipoControl)
 REFERENCES Tipo_Control(ID_TipoControl);
 
 
+ALTER TABLE Cualitativo
+ADD CONSTRAINT FK_Cualitativo_Integridad
+FOREIGN KEY (ID_Integridad)
+REFERENCES Integridad_Envase(ID_Integridad);
+
+
 ALTER TABLE Control_De_Calidad
 ADD CONSTRAINT FK_Control_TipoControl
 FOREIGN KEY (ID_TipoControl)
